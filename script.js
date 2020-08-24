2df7a9d31f6824e82bde641ca01cc1a8d03c3773
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2020-08-24 16:11:56 -0300

    create simple menu

---
+++ script.js
@@ -0,0 +1,6 @@
+function toglleMenu() {
+    let navigation = document.querySelector('.navigation')
+    let toggle = document.querySelector('.toglle')
+    navigation.classList.toggle('active')
+    toggle.classList.toglle('active')
+}
